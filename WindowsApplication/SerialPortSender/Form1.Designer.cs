8643947182efe5464a08640a349a00045660ebc9
ozturkesra <79361176+ozturkesra@users.noreply.github.com>
2021-03-07 17:51:47 +0300

    Add files via upload
    Windows Application Uploaded

---
+++ WindowsApplication/SerialPortSender/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace SerialPortSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSerialPort = new System.Windows.Forms.ComboBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbSerialPort
+            // 
+            this.cmbSerialPort.FormattingEnabled = true;
+            this.cmbSerialPort.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10"});
+            this.cmbSerialPort.Location = new System.Drawing.Point(26, 12);
+            this.cmbSerialPort.Name = "cmbSerialPort";
+            this.cmbSerialPort.Size = new System.Drawing.Size(121, 24);
+            this.cmbSerialPort.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(235, 12);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(154, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 43);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.cmbSerialPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSerialPort;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ WindowsApplication/SerialPortSender/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialPortSender
+{
+    public partial class Form1 : Form
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+        private bool isBwStopped = false;
+
+        private SerialPort firstSerialPort;
+        private SerialPort secondSerialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBackgroundWorker();
+        }
+
+        private void InitializeBackgroundWorker()
+        {
+            backgroundWorker.WorkerSupportsCancellation = true;
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(!e.Cancel)
+            {
+                if(isBwStopped)
+                {
+                    e.Cancel = true;
+                }
+
+                if(firstSerialPort.IsOpen)
+                {
+                    firstSerialPort.Write(new string(Enumerable.Repeat(chars, 1500).Select(s => s[new Random().Next(s.Length)]).ToArray()));
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            firstSerialPort = new SerialPort(cmbSerialPort.SelectedItem.ToString(), 2400);
+            firstSerialPort.Open();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if(btnSend.Text == "Send")
+            {
+                isBwStopped = false;
+                backgroundWorker.RunWorkerAsync();
+                btnSend.Text = "Stop";
+            } else
+            {
+                isBwStopped = true;
+                btnSend.Text = "Send";
+            }
+        }
+    }
+}
